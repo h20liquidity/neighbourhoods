1a6c6bb7410c054304588359b6e81a813faa085d
Siddharth2207 <siddharthbhoite22@gmail.com>
2023-05-05 17:11:59 +0530

    fix

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -44,7 +44,7 @@ const config: HardhatUserConfig = {
   solidity: {
     compilers: [
       {
-        version: "0.8.18",
+        version: "0.8.19",
         settings: {
           optimizer: {
             enabled: true,
