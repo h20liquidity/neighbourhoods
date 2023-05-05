bdac5c6265f540b60c71b4beeb03f52376e68ae8
Siddharth2207 <siddharthbhoite22@gmail.com>
2023-05-05 16:58:56 +0530

    fix

--- contracts/test/OrderBook.sol
+++ contracts/test/OrderBook.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: CAL
-pragma solidity ^0.8.18;
+pragma solidity =0.8.19;
 
 import "@rainprotocol/rain-protocol/contracts/test/testToken/ReserveToken.sol" ;
 import "@rainprotocol/rain-protocol/contracts/test/testToken/ReserveToken18.sol" ;
