c6ba321bf7af9b93a167a6697145847a71a37ded
thedavidmeister <thedavidmeister@gmail.com>
2023-08-23 01:06:57 +0400

    wip on strat

---
+++ src/3SushiV2Strat.sol
@@ -0,0 +1,64 @@
+/// @dev https://docs.sushi.com/docs/Products/Classic%20AMM/Deployment%20Addresses
+/// @dev https://polygonscan.com/address/0xc35DADB65012eC5796536bD9864eD8773aBc74C4
+address constant POLYGON_SUSHI_V2_FACTORY = 0xc35DADB65012eC5796536bD9864eD8773aBc74C4;
+
+/// @dev https://polygonscan.com/address/0x84342e932797FC62814189f01F0Fb05F52519708
+address constant POLYGON_NHT_TOKEN_ADDRESS = 0x84342e932797FC62814189f01F0Fb05F52519708;
+
+/// @dev https://polygonscan.com/address/0xc2132D05D31c914a87C6611C10748AEb04B58e8F
+address constant USDT_TOKEN_ADDRESS = 0xc2132D05D31c914a87C6611C10748AEb04B58e8F;
+
+address constant APPROVED_COUNTERPARTY = 0x1F8Cd7FB14b6930665EaaA5F5C71b9e7396df036;
+
+bytes constant RAINSTRING =
+    // String version of factory address.
+    "polygon-sushi-v2-factory: 0xc35DADB65012eC5796536bD9864eD8773aBc74C4,"
+
+    // String version of nht token address.
+    "nht-token-address: 0x84342e932797FC62814189f01F0Fb05F52519708,"
+
+    // String version of usdt token address.
+    "usdt-token-address: 0xc2132D05D31c914a87C6611C10748AEb04B58e8F,"
+
+    // String version of approved counterparty.
+    "approved-counterparty: 0x1F8Cd7FB14b6930665EaaA5F5C71b9e7396df036,"
+    "actual-counterparty: context<1 2>(),"
+
+    // Check that
+    // - counterparty is approved.
+    // - usdt token address is the token in to ob.
+    // - nht token address is the token out from ob.
+    ":ensure("
+    " equal-to(approved-counterparty actual-counterparty)"
+    " equal-to(context<3 0>() usdt-token-address)"
+    " equal-to(context<4 0>() nht-token-address)"
+    "),"
+
+    // Order hash.
+    "order-hash: context<1 0>(),"
+
+    // Figure out when the order started.
+    "order-init-time-key: hash(order-hash 0),"
+    "order-init-time: any(get(order-init-time-key) block-timestamp()),"
+    ":set(order-init-time-key order-init-time),"
+
+    // We sell $50 worth of nht for usdt per hour.
+    // 50e6 is $50 in usdt.
+    // 50e6 / 3600 is $50 per hour.
+    "usdt-per-second: 13889,"
+    "total-time: int-sub(block-timestamp() order-init-time),"
+    "max-usdt-amount: int-mul(total-time usdt-per-second),"
+    "current-usdt-amount: get(order-hash 1),"
+    "target-usdt-amount: int-sub(max-usdt-amount current-usdt-amount),"
+
+    // Token in for uniswap is ob's token out, and vice versa.
+    // We want the timestamp as well as the nht amount that sushi wants in.
+    "last-price-timestamp nht-amount: uniswap-v2-amount-in<1>(polygon-sushi-v2-factory target-usdt-amount nht-token-address usdt-token-address),"
+    // Don't allow the price to change this block before this trade.
+    ":ensure(less-than(last-price-timestamp block-timestamp())),"
+
+    // We want to sell a little more nht amount than sushi sets as the minimum
+    // to give some leeway for the arb bot.
+    "actual-nht-amount: decimal18-mul(nht-amount 1001e15),"
+    "io-ratio: decimal18-div(target-usdt-amount actual-nht-amount)"
+";";
--- test/3SushiV2Strat.t.sol
+++ test/3SushiV2Strat.t.sol
@@ -1,50 +1,94 @@
 // SPDX-License-Identifier: CAL
 pragma solidity =0.8.19;
 
-import "rain.interpreter/../test/util/abstract/OpTest.sol";
+import "rain.interpreter/test/util/abstract/OpTest.sol";
 
-/// @dev https://docs.sushi.com/docs/Products/Classic%20AMM/Deployment%20Addresses
-/// @dev https://polygonscan.com/address/0xc35DADB65012eC5796536bD9864eD8773aBc74C4
-address constant POLYGON_SUSHI_V2_FACTORY = 0xc35DADB65012eC5796536bD9864eD8773aBc74C4;
+import "src/3SushiV2Strat.sol";
 
-/// @dev https://polygonscan.com/address/0x84342e932797FC62814189f01F0Fb05F52519708
-address constant POLYGON_NHT_TOKEN_ADDRESS = 0x84342e932797FC62814189f01F0Fb05F52519708;
+// bytes constant RAINSTRING =
+//     // String version of factory address.
+//     "polygon-sushi-v2-factory: 0xc35DADB65012eC5796536bD9864eD8773aBc74C4,"
 
-/// @dev https://polygonscan.com/address/0xc2132D05D31c914a87C6611C10748AEb04B58e8F
-address constant USDT_TOKEN_ADDRESS = 0xc2132D05D31c914a87C6611C10748AEb04B58e8F;
+//     // String version of nht token address.
+//     "polygon-nht-token-address: 0x84342e932797FC62814189f01F0Fb05F52519708,"
 
-address constant APPROVED_COUNTERPARTY = 0x1F8Cd7FB14b6930665EaaA5F5C71b9e7396df036;
+//     // String version of usdt token address.
+//     "usdt-token-address: 0xc2132D05D31c914a87C6611C10748AEb04B58e8F,"
 
-bytes constant RAINSTRING =
-// String version of factory address.
-    "polygon-sushi-v2-factory: 0xc35DADB65012eC5796536bD9864eD8773aBc74C4,"
-    // String version of nht token address.
-    "polygon-nht-token-address: 0x84342e932797FC62814189f01F0Fb05F52519708,"
-    // String version of usdt token address.
-    "usdt-token-address: 0xc2132D05D31c914a87C6611C10748AEb04B58e8F,"
-    // String version of approved counterparty.
-    "approved-counterparty: 0x1F8Cd7FB14b6930665EaaA5F5C71b9e7396df036,"
-    "actual-counterparty: context<1 2>(),"
-    // Check that the counterparty is approved.
-    ":ensure(equal-to(approved-counterparty actual-counterparty)),"
-    // Order hash.
-    "order-hash: context<1 0>(),"
-    // Figure out when the order started.
-    "order-init-time-key: hash(order-hash 0),"
-    "order-init-time: any(get(order-init-time-key) block-timestamp()),"
-    ":set(order-init-time-key order-init-time),"
-    // We want the timestamp as well as the nht amount that sushi wants in.
-    "last-price-timestamp nht-amount-in: uniswap-v2-amount-in<1>(polygon-sushi-v2-factory 50e6"
-    " 0x84342e932797FC62814189f01F0Fb05F52519708" " 0xc2132D05D31c914a87C6611C10748AEb04B58e8F" ");";
+//     // String version of approved counterparty.
+//     "approved-counterparty: 0x1F8Cd7FB14b6930665EaaA5F5C71b9e7396df036,"
+//     "actual-counterparty: context<1 2>(),"
+
+//     // Check that
+//     // - counterparty is approved.
+//     // - usdt token address is the token in to ob.
+//     // - nht token address is the token out from ob.
+//     ":ensure("
+//     " equal-to(approved-counterparty actual-counterparty)"
+//     " equal-to(context<3 0>() usdt-token-address)"
+//     " equal-to(context<4 0>() polygon-nht-token-address)"
+//     "),"
+
+//     // Order hash.
+//     "order-hash: context<1 0>(),"
+
+//     // Figure out when the order started.
+//     "order-init-time-key: hash(order-hash 0),"
+//     "order-init-time: any(get(order-init-time-key) block-timestamp()),"
+//     ":set(order-init-time-key order-init-time),"
+
+//     // We sell $50 worth of nht for usdt per hour.
+//     // 50e6 is $50 in usdt.
+//     // 50e6 / 3600 is $50 per hour.
+//     "usdt-per-second: 13889,"
+
+//     // We want the timestamp as well as the nht amount that sushi wants in.
+//     "last-price-timestamp nht-amount-in: uniswap-v2-amount-in<1>(polygon-sushi-v2-factory 50e6"
+//     // " 0x84342e932797FC62814189f01F0Fb05F52519708" " 0xc2132D05D31c914a87C6611C10748AEb04B58e8F" ")"
+//     ";";
 
 contract Test3SushiV2Strat is OpTest {
-    function testStratHappyPath() public {
+    function parseAndEvalWithContext(bytes memory rainString, uint256[][] memory context) internal returns (uint256[] memory, uint256[] memory) {
+        IInterpreterV1 interpreterDeployer;
+        IInterpreterStoreV1 storeDeployer;
+        address expression;
+        {
+            (bytes memory bytecode, uint256[] memory constants) = iDeployer.parse(rainString);
+            uint256[] memory minOutputs = new uint256[](1);
+            minOutputs[0] = 0;
+            (interpreterDeployer, storeDeployer, expression) =
+                iDeployer.deployExpression(bytecode, constants, minOutputs);
+        }
+
+        (uint256[] memory stack, uint256[] memory kvs) = interpreterDeployer.eval(
+            storeDeployer,
+            StateNamespace.wrap(0),
+            LibEncodedDispatch.encode(expression, SourceIndex.wrap(0), type(uint16).max),
+            LibContext.build(context, new SignedContextV1[](0))
+        );
+        return (stack, kvs);
+    }
+
+    function testStratHappyPath(uint256 orderHash) public {
         uint256 reserveIn = 53138576564435538694955386;
         // Using USDT as an example.
-        address tokenOut = address(0xc2132D05D31c914a87C6611C10748AEb04B58e8F);
+        // address tokenOut = address(0xc2132D05D31c914a87C6611C10748AEb04B58e8F);
         uint256 reserveOut = 12270399039;
 
         uint32 reserveTimestamp = 1624291200;
+
+        uint256[][] memory context = new uint256[][](1);
+        {
+            uint256[] memory callingContext = new uint256[](3);
+            // order hash
+            callingContext[0] = orderHash;
+            // owner
+            callingContext[1] = uint256(uint160(address(this)));
+            // counterparty
+            callingContext[2] = uint256(uint160(APPROVED_COUNTERPARTY));
+            context[0] = callingContext;
+        }
+
 
         address expectedPair =
             LibUniswapV2.pairFor(POLYGON_SUSHI_V2_FACTORY, POLYGON_NHT_TOKEN_ADDRESS, USDT_TOKEN_ADDRESS);
@@ -53,13 +97,32 @@ contract Test3SushiV2Strat is OpTest {
             abi.encodeWithSelector(IUniswapV2Pair.getReserves.selector),
             abi.encode(reserveIn, reserveOut, reserveTimestamp)
         );
-        (uint256[] memory stack, uint256[] memory kvs) = parseAndEval(RAINSTRING);
+        (uint256[] memory stack, uint256[] memory kvs) = parseAndEvalWithContext(RAINSTRING, context);
 
-        assertEq(stack.length, 5);
+        assertEq(kvs.length, 2);
+
+        assertEq(stack.length, 8);
+        // addresses.
         assertEq(stack[0], uint256(uint160(POLYGON_SUSHI_V2_FACTORY)));
         assertEq(stack[1], uint256(uint160(POLYGON_NHT_TOKEN_ADDRESS)));
         assertEq(stack[2], uint256(uint160(USDT_TOKEN_ADDRESS)));
-        assertEq(stack[3], reserveTimestamp);
-        assertEq(stack[4], 218071733215424131376675);
+
+        // approved counterparty.
+        assertEq(stack[3], uint256(uint160(APPROVED_COUNTERPARTY)));
+        // actual counterparty.
+        assertEq(stack[4], uint256(uint160(APPROVED_COUNTERPARTY)));
+
+        // order init time key.
+        assertEq(stack[5], orderHash);
+        assertEq(stack[6], uint256(keccak256(abi.encodePacked(orderHash, uint256(0)))));
+        assertEq(stack[7], block.timestamp);
+
+
+        // assertEq(stack[3], reserveTimestamp);
+        // assertEq(stack[4], 218071733215424131376675);
+    }
+
+    function testStratDebug() external {
+        testStratHappyPath(0);
     }
 }
