040eb8ec9477a481991fa35947856e837bace395
Siddharth2207 <siddharthbhoite22@gmail.com>
2023-12-08 19:18:07 +0530

    Update Limit Strategy String

--- scripts-v3/DeployStrategy/limitStrat.ts
+++ scripts-v3/DeployStrategy/limitStrat.ts
@@ -2,26 +2,26 @@ import config from "../v3-config.json";
 
 
 const TRANCHE_STRAT_CALCULATE_BATCH =
-// Amount received
-    "new-received: ,"+
-    // Total amount received key
-    "total-received-k: hash(context<1 0>()),"+
+// Usdt18 Amount
+    "usdt18-amount: ,"+
+    // Total Usdt18 Key
+    "total-usdt18-k: hash(context<1 0>()),"+
     // Amount Per Batch
     "amount-per-batch: 100e18,"+
     // New total amount
-    "new-total-amount-received: decimal18-add(get(total-received-k) new-received),"+
+    "new-total-usdt18-amount: decimal18-add(get(total-usdt18-k) usdt18-amount),"+
     // Batch Index is the floor of the div
-    "new-batch-index: int-div(new-total-amount-received amount-per-batch),"+
+    "new-batch-index: int-div(new-total-usdt18-amount amount-per-batch),"+
     // Remaining batch amount
-    "new-batch-remaining: decimal18-sub(int-mul(int-add(new-batch-index 1) amount-per-batch) new-total-amount-received);";
+    "new-batch-remaining: decimal18-sub(int-mul(int-add(new-batch-index 1) amount-per-batch) new-total-usdt18-amount);";
 
 const TRANCHE_STRAT_HANDLE_IO =
     // Batch Info Key.
         "batch-start-info-k : context<1 0>(),"+
-        // Total Amount Received Key
-        "total-received-k : hash(batch-start-info-k),"+
-        // New Total Amount Received
-        "new-total-received new-batch-index _: call<2 3>(decimal18-scale18<6>(usdt-amount-diff)),"+
+        // Total Usdt18 Tracked Key
+        "total-usdt18-k : hash(batch-start-info-k),"+
+        // New Total Usdt18
+        "new-total-usdt18 new-batch-index _: call<2 3>(decimal18-scale18<6>(usdt-amount-diff)),"+
         // Get Batch Info
         "batch-start-info: get(batch-start-info-k),"+
         // Get Batch Start Index from Batch Info.
@@ -32,8 +32,8 @@ const TRANCHE_STRAT_HANDLE_IO =
         "new-batch-info : if(greater-than(new-batch-index batch-start-index) bitwise-encode<32 32>(block-timestamp() bitwise-encode<0 32>(new-batch-index 0)) batch-start-info),"+
         // Set Batch Info.
         ":set(batch-start-info-k new-batch-info),"+
-        // Set Total Amount received.
-        ":set(total-received-k new-total-received);";
+        // Set Total Usdt18 
+        ":set(total-usdt18-k new-total-usdt18);";
 
 export const getLimitOrderPrelude = (network) => {
 
@@ -72,7 +72,7 @@ export const getSellLimitOrder = (network,sellRatio) => {
     "ratio: io-ratio;"; 
 
     const TRANCHE_STRAT_HANDLE_IO_SELL_USDT_IN =
-    // Input Amount received.
+    // Input USDT Amount.
     "usdt-amount-diff : context<3 4>(),";
     return getLimitOrderPrelude(network) + TRANCHE_STRAT_CALCULATE_IO_SELL + TRANCHE_STRAT_HANDLE_IO_SELL_USDT_IN + TRANCHE_STRAT_HANDLE_IO + TRANCHE_STRAT_CALCULATE_BATCH
 
@@ -90,7 +90,7 @@ export const getBuyLimitOrder = (network,buyRatio) => {
     "ratio: io-ratio;";
 
     const TRANCHE_STRAT_HANDLE_IO_BUY_USDT_OUT =
-    // USDT Amount Sent.
+    // Output USDT Amount.
     "usdt-amount-diff : context<4 4>(),";
 
     
