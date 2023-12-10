9d1eaf6cb7165e762c9ed2cee0d5ce5eecc25636
Siddharth2207 <siddharthbhoite22@gmail.com>
2023-12-10 18:12:27 +0530

    fmt

--- test/TestForkParser.t.sol
+++ test/TestForkParser.t.sol
@@ -5,10 +5,7 @@ import {Test, console2} from "forge-std/Test.sol";
 import "src/interface/IParserV1.sol";
 
 import {
-    rainstringBuy,
-    rainstringSell,
-    EXPECTED_SELL_BYTECODE,
-    EXPECTED_BUY_BYTECODE
+    rainstringBuy, rainstringSell, EXPECTED_SELL_BYTECODE, EXPECTED_BUY_BYTECODE
 } from "src/4SushiV2StratBinomial.sol";
 
 import {
@@ -16,13 +13,12 @@ import {
     rainstringSellLimitOrder,
     EXPECTED_SELL_LIMIT_BYTECODE,
     EXPECTED_BUY_LIMIT_BYTECODE
-} from "src/5SushiV2LimitOrder.sol"; 
+} from "src/5SushiV2LimitOrder.sol";
 
 /// @dev https://mumbai.polygonscan.com/address/0xDaAB45E4BCCEbcE8d84995E41CC251C6F9a92aFD
 /// CI : https://github.com/rainlanguage/rain.interpreter/actions/runs/7156612121/job/19486513484
 /// Commit Hash : https://github.com/rainlanguage/rain.interpreter/tree/32dc48b362630c9282ea1245fb0185449d90f67c
 IParserV1 constant PARSER_NEW = IParserV1(0xDaAB45E4BCCEbcE8d84995E41CC251C6F9a92aFD);
-
 
 contract TestForkParser is Test {
     string constant FORK_RPC = "https://rpc.ankr.com/polygon_mumbai";
@@ -35,11 +31,11 @@ contract TestForkParser is Test {
     }
 
     function testVolumeParsedBytecodes() public {
-        selectPolygonFork(); 
+        selectPolygonFork();
 
         // Sell Vol Order
         (bytes memory sellBytecode,) = PARSER_NEW.parse(rainstringSell());
-        assertEq(sellBytecode, EXPECTED_SELL_BYTECODE); 
+        assertEq(sellBytecode, EXPECTED_SELL_BYTECODE);
 
         // Buy Vol Order
         (bytes memory buyBytecode,) = PARSER_NEW.parse(rainstringBuy());
@@ -47,11 +43,11 @@ contract TestForkParser is Test {
     }
 
     function testLimitParsedBytecodes() public {
-        selectPolygonFork(); 
+        selectPolygonFork();
 
         // Sell Limit Order
         (bytes memory sellBytecode,) = PARSER_NEW.parse(rainstringSellLimitOrder());
-        assertEq(sellBytecode, EXPECTED_SELL_BYTECODE); 
+        assertEq(sellBytecode, EXPECTED_SELL_BYTECODE);
 
         // Buy Limit Order
         (bytes memory buyBytecode,) = PARSER_NEW.parse(rainstringBuyLimitOrder());
